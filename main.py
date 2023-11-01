9c8041f94142a0a0f76143ddfc9a80d426ae92b2
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2023-11-01 12:24:30 +0100

    Refactored the domain objects to have separate file for readability

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
-from POMDPService import POMDP_AJAN
+from POMDPService.interface.pomdp import pomdp_ns
+from POMDPService.interface.state import state_ns
+from POMDPService.interface.action import action_ns
+from POMDPService.interface.belief import belief_ns
 
 app = FastAPI(title="AJAN-POMDP", description="API for planning with pomdp-py through AJAN")
-app.include_router(POMDP_AJAN.pomdp_ns, tags=['POMDP Service'])
-app.include_router(POMDP_AJAN.state_ns, tags=['State'])
-app.include_router(POMDP_AJAN.action_ns, tags=['Action'])
-app.include_router(POMDP_AJAN.belief_ns, tags=['Belief'])
+app.include_router(pomdp_ns, tags=['POMDP Service'])
+app.include_router(state_ns, tags=['State'])
+app.include_router(action_ns, tags=['Action'])
+app.include_router(belief_ns, tags=['Belief'])
 
 app1 = FastAPI(title="AJAN-POMDP-State", description="API for planning with pomdp-py through AJAN1")
-app1.include_router(POMDP_AJAN.pomdp_ns, tags=['POMDP Service1'])
+app1.include_router(pomdp_ns, tags=['POMDP Service1'])
 
 
 @app.on_event("startup")
