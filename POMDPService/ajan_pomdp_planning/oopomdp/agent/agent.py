a81167b0a15461ea50f0792a15847a0af1eb77e6
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2023-11-05 15:15:16 +0100

    Created the agent

---
+++ POMDPService/ajan_pomdp_planning/oopomdp/agent/agent.py
@@ -0,0 +1,12 @@
+import pomdp_py
+from rdflib import Graph
+
+
+class AjanAgent(pomdp_py.Agent):
+    def __init__(self, data, init_belief, policy_model, transition_model, observation_model, reward_model):
+        self.graph = Graph()
+        self.graph.parse(data)
+        super().__init__(init_belief, policy_model=policy_model,
+                         transition_model=transition_model,
+                         observation_model=observation_model,
+                         reward_model=reward_model)
