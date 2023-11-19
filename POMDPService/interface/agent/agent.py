9a7ed4c8c53060373c659e5df4c21ee9d027c336
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2023-11-19 19:29:32 +0100

    Can now generate the observation but needs some changes for efficient query later

--- POMDPService/interface/agent/agent.py
+++ POMDPService/interface/agent/agent.py
@@ -1,14 +1,17 @@
 import ctypes
 
+import rdflib.term
+import rdfpandas
 from fastapi import APIRouter
-from rdflib import Graph
+from rdflib import Graph, RDF, XSD
 
 from POMDPService.VariableModels.ResponseModels import CreateResponse, BooleanResponse
 from POMDPService.VariableModels.State import AgentInit
 from POMDPService.ajan_pomdp_planning.oopomdp.agent.agent import AjanAgent
+from POMDPService.ajan_pomdp_planning.oopomdp.domain.observation import AjanObservation
 from POMDPService.ajan_pomdp_planning.oopomdp.problem import AjanOOPOMDP
 from POMDPService.interface.pomdp import init_beliefs, models, agents, problems, last_action, last_observation
-from POMDPService.ajan_pomdp_planning.vocabulary.POMDPVocabulary import _CurrentObservation
+from POMDPService.ajan_pomdp_planning.vocabulary.POMDPVocabulary import _CurrentObservation, pomdp_ns1, pomdp_ns, _Type
 
 agent_ns = APIRouter(prefix="/AJAN/pomdp/agent")
 
@@ -48,15 +51,45 @@ def clear_history(pomdp_id):
 def update_history(pomdp: AgentInit):
     pomdp_id = pomdp.pomdp_id
     # Create and observation and then update the history
-    create_observation(pomdp.data)
+    observation = create_observation(pomdp.data)
     problem: AjanOOPOMDP = problems[pomdp_id]
-    problem.agent.update_history(last_action[pomdp_id], last_observation[pomdp_id])
+    problem.agent.update_history(last_action[pomdp_id], observation)
     return BooleanResponse(success=True, message="Agent history updated successfully")
+
+
+g = Graph()
 
 
 def create_observation(data):
     # Create an observation
-    g = Graph()
     g.parse(data=data)
-    for s, p, o in g.triples((_CurrentObservation, None, None)):
-        print(s, p, o)
+    keys = [o for s, p, o in g.triples((_CurrentObservation, RDF.value, None))]
+    convert_function = {}
+    attributes = {}
+    for key in keys:
+        for s, p, o in g.triples((key, _Type, None)):
+            convert_function[get_key_value(str(key))] = get_parse_function(s, o)
+        for s, p, value_node in g.triples((key, RDF.value, None)):
+            attributes[get_key_value(str(key))] = convert_function[get_key_value(str(key))](value_node)
+    return AjanObservation(attributes, ['id'])
+
+
+def get_key_value(key: str):
+    return key.replace(str(pomdp_ns1['data']) + "/", "")
+
+
+def get_parse_function(s, datatype: rdflib.term.Node):
+    if datatype == pomdp_ns1['data/pandasDataFrame']:
+        return get_dataframe_from_observation
+    func = rdflib.term.XSDToPython[datatype]
+    if func is None:
+        return str
+    return func
+
+
+def get_dataframe_from_observation(observation):
+    df_graph = Graph()
+    for s, p, o in g.triples((observation, RDF.value, None)):
+        for s1, p1, o1 in g.triples((o, None, None)):
+            df_graph.add((s1, p1, o1))
+    return rdfpandas.to_dataframe(df_graph)
