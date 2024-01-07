a1a3a0d1132c7ead3989e36e3686db15a716fe66
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2024-01-07 07:14:01 +0100

    Added test for drone transition model probability

---
+++ tests/test_models/test_transition_model/test_drone_transition_model_probability.py
@@ -0,0 +1,201 @@
+import unittest
+
+from POMDPService.ajan_pomdp_planning.oopomdp.domain.action import AjanAction
+from POMDPService.ajan_pomdp_planning.oopomdp.domain.state import AjanAgentState, AjanOOState
+from POMDPService.ajan_pomdp_planning.oopomdp.models.transition_model import AjanTransitionModel
+from tests.test_models.helpers.drone_transition_model_queries import DATA, SAMPLE_QUERY, ARGMAX_QUERY, PROBABILITY_QUERY
+
+
+class TestDroneTransitionModelProbability(unittest.TestCase):
+
+    drone_transition_model = AjanTransitionModel(100, DATA, None, PROBABILITY_QUERY, SAMPLE_QUERY, ARGMAX_QUERY)
+
+
+    def test_probability_true_true_perceive(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        action = AjanAction("perceive")
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+    def test_probability_true_false_perceive(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        action = AjanAction("perceive")
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-09)
+
+    def test_probability_false_true_perceive(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        action = AjanAction("perceive")
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-09)
+
+    def test_probability_false_false_perceive(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        action = AjanAction("perceive")
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+    def test_probability_none_true_perceive(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("perceive")
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-09)
+
+    def test_probability_none_false_perceive(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("perceive")
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-09)
+
+    def test_probability_none_none_perceive(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("perceive")
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+    def test_probability_true_none_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_false_none_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_true_true_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+    def test_probability_true_false_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_false_true_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_false_false_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False},
+                                                      ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+    def test_probability_none_true_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_none_false_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_none_none_move_right(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "right"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+    def test_probability_true_true_move_left(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "left"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_false_true_move_left(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "left"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_true_false_move_left(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "left"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_false_false_move_left(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "left"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+    def test_probability_none_true_move_left(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "left"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_none_false_move_left(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "left"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": False}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+
+    def test_probability_none_none_move_left(self):
+        current_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                         {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        action = AjanAction("move", {"motion": "left"})
+        next_state = AjanOOState({100: AjanAgentState("Drone", 100,
+                                                      {"id": 100, "pose": (0, 0), "gesture_found": None}, ["pose", "gesture_found"])})
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
+
+
+if __name__ == '__main__':
+    unittest.main()
