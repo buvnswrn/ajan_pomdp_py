9fd80aae65c6493d9bc20618863768a014f4773d
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2024-01-07 09:53:36 +0100

    bugfix: fixed issues in the probability query of the drone transition model

--- tests/test_models/test_transition_model/test_drone_transition_model_probability.py
+++ tests/test_models/test_transition_model/test_drone_transition_model_probability.py
@@ -146,7 +146,7 @@ class TestDroneTransitionModelProbability(unittest.TestCase):
         action = AjanAction("move", {"motion": "left"})
         next_state = AjanOOState({100: AjanAgentState("Drone", 100,
                                                       {"id": 100, "pose": (0, 0), "gesture_found": True}, ["pose", "gesture_found"])})
-        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 1e-9)
+        self.assertEqual(self.drone_transition_model.probability(next_state, current_state, action), 0.999999999)
 
     def test_probability_false_true_move_left(self):
         current_state = AjanOOState({100: AjanAgentState("Drone", 100,
