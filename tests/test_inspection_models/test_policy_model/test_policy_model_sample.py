fa52ee025e293bca341a77bd38e55045c41f8ae7
Bhuvaneshwaran Ilanthirayan <buvnswrn@gmail.com>
2024-01-31 19:32:59 +0100

    get_all_actions query and test cases added

--- tests/test_inspection_models/test_policy_model/test_policy_model_sample.py
+++ tests/test_inspection_models/test_policy_model/test_policy_model_sample.py
@@ -8,7 +8,8 @@ from tests.test_inspection_models.helpers.policy_model_queries import DATA_POLIC
 class TestPolicyModelSample(unittest.TestCase):
 
     policy_model = AjanPolicyModel(DATA_POLICY, None, SAMPLE_QUERY_POLICY, ROLLOUT_QUERY_POLICY, GET_ALL_ACTIONS_QUERY)
-    def test_something(self):
+
+    def test_sample_2_left_false_2_0(self):
         self.assertEqual(True, False)  # add assertion here
 
 
